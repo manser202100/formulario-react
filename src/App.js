feecdaaf8ee2513f2717740afd891a323c04dd1d
manser202100 <serviciomessage@gmail.com>
2021-11-30 11:31:00 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ContactUs from './components/contactForm';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        <ContactUs/>
+      </div>
+    </div>
+  );
+}
+
+export default App;
